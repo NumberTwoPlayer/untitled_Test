e32e4136e51cda18ab06ba80ea64fc34c8017066
刘梹晨 <3394427855@qq.com>
2022-08-18 23:09:13 +0800

    Changes

--- 33944/IDEA_Java/ideaProjects/untitled_Test/src/code/Optimize/Sort/BubbleSort.java
+++ 33944/IDEA_Java/ideaProjects/untitled_Test/src/code/Optimize/Sort/BubbleSort.java
@@ -1,7 +1,18 @@
 package code.Optimize.Sort;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(int[] arr) {
+        for(int i = arr.length - 1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                if(arr[j] < arr[j + 1]){
+                    swap(arr, arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
+    public static void swap(int[] arr, int i, int j){
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
     }
 }
--- 33944/IDEA_Java/ideaProjects/untitled_Test/src/code/Optimize/Sort/QuickSort.java
+++ 33944/IDEA_Java/ideaProjects/untitled_Test/src/code/Optimize/Sort/QuickSort.java
@@ -2,6 +2,6 @@ package code.Optimize.Sort;
 
 public class QuickSort {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
     }
 }
